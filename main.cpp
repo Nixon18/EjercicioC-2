fd5fdd346247aaf21fe217446189c22a9ad64039
nixon <nixonmartinezvalencia@gmail.com>
2020-04-20 14:18:33 -0500

    se subio segunda evidencia de c++ camara detectora covid

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <conio.h>
+
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	float temperatura;
+	
+	cout<<"Ingreso a la camara detectora del segundo sintoma del covid-19"<<endl;
+	cout<<"------------------------------------------------------------------------"<<endl;	
+	cout<<"Por favor ingrese su temperatura"<<endl;
+	cin>>temperatura;
+	
+	
+	
+	if(temperatura >= 37){
+		
+		cout<<"necesitas chequeo de un medico, pues tienes uno de los sintomas de virus"<<endl;
+		
+	}else{
+		
+		cout<<"cuidate"<<endl;
+	}
+	
+	
+
+	
+	
+	return 0;
+}
